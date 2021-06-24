9bf1cf404fe673bbf9a7e8e9f11ffc572809d45a
MedAmineKthiri <medaminekthiri@gmail.com>
2021-06-24 12:36:27 +0200

    noth

---
+++ main.js
@@ -0,0 +1,64 @@
+var dog =[
+ {
+	breed:'German Shepherd',
+	name:'Cesar',
+	age:'2years'
+	 src:"./images/german shepherd"
+     },
+     {
+    breed:'malinois',
+	name:'Cesar',
+	age:'2years'
+	src:"./images/malinois"
+
+     },
+     {
+    breed:'French buldog',
+	name:'Blaki',
+	age:'3years'
+	src:"./images/french buldog" 	
+     },
+     {
+      breed:'Yorkshire',
+	name:'Blaki',
+	age:'3years'
+	src:"./images/yorkshire" 	
+     },
+    {
+    breed:'Akita',
+	name:'Rigen',
+	age:'5 months'
+	src:"./images/akita"	
+    } 
+    ]
+
+
+
+var cat =[
+     {
+	name:'Blondi',
+	age:'2years'
+	src:"./images/cat1"
+     },
+     {
+	name:'Jaki',
+	age:'2years'
+	src:"./images/cat2"
+
+     },
+     {
+	name:'star',
+	age:'3years'
+	src:"./images/cat3"	
+     },
+    {
+	name:'Moon',
+	age:'5 months'
+	src:"./images/cat4"	
+    },
+    {
+    name:'Tom',
+	age:'5 months'
+	src:"./images/cat5"	
+    }
+    ]
