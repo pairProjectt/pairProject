61f085b2929ae490cb009c6740088c5beeba0054
MedAmineKthiri <medaminekthiri@gmail.com>
2021-06-25 17:59:13 +0200

    done

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 var dogs =[
- {
-	breed:'German Shepherd',
+ {	breed:'German Shepherd',
 	name:'Cesar',
-	age:'2years'
+	age:'2years',
 	 src:"./images/german shepherd.jpg"
      },
      {
     breed:'malinois',
-	name:'Cesar',
-	age:'2years'
+	name:'Crowdy',
+	age:'2years',
 	src:"./images/malinois.jpg"
 
      },
      {
     breed:'French buldog',
 	name:'Blaki',
-	age:'3years'
+	age:'3years',
 	src:"./images/french buldog.jpg" 	
      },
      {
       breed:'Yorkshire',
-	name:'Blaki',
-	age:'3years'
+	name:'Starski',
+	age:'3years',
 	src:"./images/yorkshire.jpg" 	
      },
     {
     breed:'Akita',
 	name:'Rigen',
-	age:'5 months'
+	age:'5 months',
 	src:"./images/akita.jpg"	
     } 
     ]
@@ -37,53 +36,77 @@ var dogs =[
 var cats =[
      {
 	name:'Blondi',
-	age:'2years'
+	age:'2years',
 	src:"./images/cat1.jpg"
      },
      {
 	name:'Jaki',
-	age:'2years'
+	age:'2years',
 	src:"./images/cat2.jpg"
 
      },
      {
-	name:'star',
-	age:'3years'
+	name:'Star',
+	age:'3years',
 	src:"./images/cat3.jpg"	
      },
     {
 	name:'Moon',
-	age:'5 months'
+	age:'5 months',
 	src:"./images/cat4.jpg"	
     },
     {
     name:'Tom',
-	age:'5 months'
+	age:'5 months',
 	src:"./images/cat5.jpg"	
     }
     ]
 
+   $( ".btn" ).click(function( event ) {
+   	        console.log(event.target.id)
+	 for(var i=0;i<dogs.length;i++){
+	 	if (dogs[i].name== event.target.id){
+	 	var str = "Congrats for adopting " +dogs[i].name+" "+dogs[i].age + " "+" we will contact you via mail"
+	 		alert( str )
+	 	}
 
-   $( "#dogg" ).click(function( event ) {
- for(var i=0;i<dogs.length;i++){
- 	if (dogs[i].model === event.target.id){
- 		var $dog=$(`<div><p> model ${dogs[i].model}</p> <img src=${dogs[i].src} /> </div>`)
- 		
- 	}
- 	$('#dogg').html($dog)
-
-    }
+	    }
  });
 
 
+$( ".btn" ).click(function( event ) {
+   	        console.log(event.target.id)
+	 for(var i=0;i<cats.length;i++){
+	 	if (cats[i].name== event.target.id){	 	
+	 	var str = "Congrats for adopting " +cats[i].name+" "+cats[i].age + " "+" we will contact you via mail"
+	 		alert( str )
+	 	}
+	 	
 
-    $( "a" ).click(function( event ) {
- for(var i=0;i<cats.length;i++){
- 	if (cats[i].model === event.target.id){
- 		var $cats=$(`<div><p> model ${cats[i].model}</p> <img src=${cats[i].src} /> </div>`)
- 		
- 	}
- 	$('#cat').html($cat)
-
-    }
+	    }
  });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //    $( "a" ).click(function( event ) {
+ // for(var i=0;i<cats.length;i++){
+ // 	if (cats[i].name === event.target.id){
+ // 		var $cats=$(`<div><p> name ${cats[i].name}</p> <img src=${cats[i].src} /> </div>`)
+ 		
+ // 	}
+ // 	$('#cat').html($cat)
+
+ //    }
+ // });
