e2243438ddfed6b1ccfe0dc9064884315a43784d
tabka.sahar <tabkasahar3@gmail.com>
2021-06-25 13:03:26 +0200

    add function

--- main.js
+++ main.js
@@ -28,15 +28,36 @@ function signUp (username,password,email){
 	obj.email = email
 	users.push(obj)
 	localStorage.setItem("users",JSON.stringify(users))
+	window.location.replace("file:///C:/Users/sahar/OneDrive/Bureau/pairProject-1/page2.html")
+	alert ('welcome')
 	return obj
 }
+
+window.onload = randomPic;
+var myImage=newArray ("C:\Users\sahar\OneDrive\Bureau\pairProject-1\images\gettyimages-172050389_global-usage_0046.jpg","C:\Users\sahar\OneDrive\Bureau\pairProject-1\images\animal-sitting-animals-inside.jpg","C:\Users\sahar\OneDrive\Bureau\pairProject-1\images\10-Best-Names-For-Black-Cats.jpg")
+function randomPic(){
+	var randomNum = (Math.floor(Math.random)*myImage.length)
+	document.getElementByClassName("column").src = myImage[randomNum]
+	src = 
+
+}
+ 
+
+
+
+
+
  function login ( username,password){
  	for (var i = 0; i<users.length; i++){
+ 		debugger
+ 		console.log(users[i].name)
  		if (username === users[i].name && password === users[i].password){
  			alert ('welcome')
- 		
+
+ 			// window.location.replace("file:///C:/Users/sahar/OneDrive/Bureau/pairProject-1/page2.html")
  	}else {
  		return location.reload()
+ 		function.hide ()
  	}
  }
 }
@@ -45,23 +66,5 @@ function signUp (username,password,email){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var obj = {username:'sahar', password : 123456}
 
 
